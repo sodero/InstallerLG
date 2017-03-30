acfc14b40ed4927264db8a1c43ee9aab083baaf7
Rolf Kopman <rolfkopman@gmail.com>
2017-03-30 22:09:45 +0200

    Simple tests

---
+++ test/run.sh
@@ -0,0 +1,46 @@
+exe()
+{
+    return 0
+}
+
+out()
+{
+    if [ `echo $1 | $prg` = "$2" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+evl()
+{
+    t=`echo "$2" | sed -e 's/=.*$//'`
+    w=`echo "$2" | sed -e 's/.*=\"\(.*\)\".*/\1/'`
+    if [ $t = "out" ]; then 
+        out "$1" "$w" 
+        return $?
+    elif [ $t = "exe" ]; then
+        exe "$1" "$w"
+        return $?
+    else
+        return 0
+    fi
+}
+
+prg=$1
+tst=$2
+for f in $tst/test.*; 
+do 
+    echo "-> $f"
+    while read -r l; 
+    do
+       p=`echo "$l" | sed -e 's/;.*$//'`
+       r=`echo "$l" | sed -e 's/.*;//'`
+       evl "$p" "$r" 
+       if [ $? = 1 ]; then
+           echo "OK -> $p" 
+       else 
+           echo "FAIL -> $p"
+       fi
+    done < $f
+done
