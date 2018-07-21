84a1fe9d01ad79c4e4d35443f9704bf22ee685c5
Rolf Kopman <rolfkopman@gmail.com>
2018-07-21 21:57:32 +0200

    Status update

--- src/init.c
+++ src/init.c
@@ -33,7 +33,7 @@
 
 static char version[] __attribute__((used)) = "\0$VER: InstallerLG " 
                                                VER(MAJOR) "." VER(MINOR) 
-                                              " [ALPHA12]";
+                                              " [ALPHA13]";
 
 //----------------------------------------------------------------------------
 // Name:        init
