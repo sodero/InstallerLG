fd9d8e7e122aa8c424506cc56332c8c5a9d032f6
Rolf Kopman <rolfkopman@gmail.com>
2018-08-25 22:30:30 +0200

    Status update

--- src/init.c
+++ src/init.c
@@ -37,7 +37,7 @@
 
 static char version[] __attribute__((used)) = "\0$VER: InstallerLG " 
                                                VER(MAJOR) "." VER(MINOR) 
-                                              " [ALPHA25]";
+                                              " [ALPHA26]";
 
 //----------------------------------------------------------------------------
 // Name:        native_exists
