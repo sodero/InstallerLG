2bbd301b2492f0d65f74163628d872ef8570ed57
Ola Söder <olasr@axis.com>
2018-12-28 18:13:28 +0100

    Sed workaround'

--- test/run.sh
+++ test/run.sh
@@ -67,8 +67,9 @@ evl()
         massfile=`mktemp ./massif.out.XXXXXX`
         echo "$1 ; [$pre ; $pst]" > $instfile
         valgrind --tool=massif --massif-out-file=$massfile --stacks=yes $prg $instfile > /dev/null 2>&1
-        sed -i -e "s/mem_stacks_B=/$1 mem_stacks_B=/" $massfile > /dev/null 2>&1
-        sed -i -e "s/mem_heap_B=/$1 mem_heap_B=/" $massfile > /dev/null 2>&1
+        snip=$(echo $1 | tr -d '.' | tr '/' '_' | tr -d '"')
+        sed -i "s/mem_stacks_B=/$snip mem_stacks_B=/g" $massfile
+        sed -i "s/mem_heap_B=/$snip mem_heap_B=/g" $massfile
         if [ -n "$pst" ]; then
             eval "$pst" 2>&1
         fi
