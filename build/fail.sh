a631113b67aaa7d14ca1265a97c811d5bde5b7eb
Ola Söder <rolfkopman@gmail.com>
2020-07-16 23:29:54 +0200

    Enable fopen failures in fail scripts.

--- build/fail.sh
+++ build/fail.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-numb=$(grep DBG_ALLOC ../src/*.c -n | wc -l)
-file=$(grep DBG_ALLOC ../src/*.c -n | awk -F: '{print $1}')
-line=$(grep DBG_ALLOC ../src/*.c -n | awk -F: '{print $2}')
-numb=$(awk -v min=1 -v max="$numb" 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
-file=$(echo "$file" | head -n $numb | tail -n 1)
-line=$(echo "$line" | head -n $numb | tail -n 1)
+alloc_numb=$(grep DBG_ALLOC ../src/*.c -n | wc -l)
+alloc_file=$(grep DBG_ALLOC ../src/*.c -n | awk -F: '{print $1}')
+alloc_line=$(grep DBG_ALLOC ../src/*.c -n | awk -F: '{print $2}')
+alloc_numb=$(awk -v min=1 -v max="$alloc_numb" 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+alloc_file=$(echo "$alloc_file" | head -n $alloc_numb | tail -n 1)
+alloc_line=$(echo "$alloc_line" | head -n $alloc_numb | tail -n 1)
+fopen_numb=$(grep DBG_FOPEN ../src/*.c -n | wc -l)
+fopen_file=$(grep DBG_FOPEN ../src/*.c -n | awk -F: '{print $1}')
+fopen_line=$(grep DBG_FOPEN ../src/*.c -n | awk -F: '{print $2}')
+fopen_numb=$(awk -v min=1 -v max="$fopen_numb" 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+fopen_file=$(echo "$fopen_file" | head -n $fopen_numb | tail -n 1)
+fopen_line=$(echo "$fopen_line" | head -n $fopen_numb | tail -n 1)
+
 echo "---------------------------------------------------------------------"
-echo "Memory allocation fault injection in $file line $line"
+echo "MALLOC fault injection in $alloc_file line $alloc_line"
+echo "FOPEN file fault injection in $fopen_file line $fopen_line"
 echo "---------------------------------------------------------------------"
-make clean &> /dev/null
-make CFLAGS="-I . -I ../src -g -std=c99 -D_DEFAULT_SOURCE -DFAIL_LINE=$line -DFAIL_FILE='\"$file\"'" &> /dev/null
-make fail
+make clean
+make CFLAGS="-I . -I ../src -g -std=c99 -D_DEFAULT_SOURCE -D_GNU_SOURCE -D__AMIGADATE__='\"23.07.1985\"' -DFAIL_LINE_ALLOC=$alloc_line -DFAIL_FILE_ALLOC='\"$alloc_file\"' -DFAIL_LINE_FOPEN=$fopen_line -DFAIL_FILE_FOPEN='\"$fopen_file\"'" &> /dev/null
+test -e ./Installer && make fail || echo '>> BUILD ERROR <<' && exit 8
 ls leak.tmp.* &> /dev/null && cat leak.tmp.* && exit 1
-ls err.tmp.* &> /dev/null && echo $(ls -l err.tmp.* 2> /dev/null | wc -l) "fault(s) handled without memory violations or leaks." && exit 0
+ls err.tmp.* &> /dev/null && echo $(ls -l err.tmp.* 2> /dev/null | wc -l) "error(s) handled without leaks." && exit 0
 echo "No errors and no leaks."
 exit 0
--- build/fail_this.sh
+++ build/fail_this.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-if [ $# -ne 2 ]
+if [ $# -ne 4 ]
 then
-    echo "Usage: fail_this file line"
+    echo "Usage: fail_this file_alloc line_alloc file_fopen line_fopen"
     exit 1
 fi
-numb=$(grep DBG_ALLOC ../src/*.c -n | wc -l)
-file=$(grep DBG_ALLOC ../src/*.c -n | awk -F: '{print $1}')
-line=$(grep DBG_ALLOC ../src/*.c -n | awk -F: '{print $2}')
-numb=$(awk -v min=1 -v max="$numb" 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
-file=$1
-line=$2
+
+alloc_file=$1
+alloc_line=$2
+fopen_file=$3
+fopen_line=$4
+
 echo "---------------------------------------------------------------------"
-echo "Memory allocation fault injection in $file line $line"
+echo "MALLOC fault injection in $alloc_file line $alloc_line"
+echo "FOPEN file fault injection in $fopen_file line $fopen_line"
 echo "---------------------------------------------------------------------"
 make clean
-make CFLAGS="-I . -I ../src -g -std=c99 -D_DEFAULT_SOURCE -D_GNU_SOURCE -DFAIL_LINE=$line -DFAIL_FILE='\"$file\"'"
-make fail
+make CFLAGS="-I . -I ../src -g -std=c99 -D_DEFAULT_SOURCE -D_GNU_SOURCE -D__AMIGADATE__='\"23.07.1985\"' -DFAIL_LINE_ALLOC=$alloc_line -DFAIL_FILE_ALLOC='\"$alloc_file\"' -DFAIL_LINE_FOPEN=$fopen_line -DFAIL_FILE_FOPEN='\"$fopen_file\"'" &> /dev/null 
+test -e ./Installer && make fail || echo '>> BUILD ERROR <<' && exit 8
 ls leak.tmp.* &> /dev/null && cat leak.tmp.* && exit 1
 ls err.tmp.* &> /dev/null && echo $(ls -l err.tmp.* 2> /dev/null | wc -l) "error(s) handled without leaks." && exit 0
 echo "No errors and no leaks."
