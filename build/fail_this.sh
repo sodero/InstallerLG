a00c449b0007412d6e4e7fbfba58c34d68229a19
Ola Söder <rolfkopman@gmail.com>
2020-07-17 00:20:30 +0200

    Don't silence compiler output.

--- build/fail_this.sh
+++ build/fail_this.sh
@@ -15,8 +15,8 @@ echo "MALLOC fault injection in $alloc_file line $alloc_line"
 echo "FOPEN file fault injection in $fopen_file line $fopen_line"
 echo "---------------------------------------------------------------------"
 make clean
-make CFLAGS="-I . -I ../src -g -std=c99 -D_DEFAULT_SOURCE -D_GNU_SOURCE -D__AMIGADATE__='\"23.07.1985\"' -DFAIL_LINE_ALLOC=$alloc_line -DFAIL_FILE_ALLOC='\"$alloc_file\"' -DFAIL_LINE_FOPEN=$fopen_line -DFAIL_FILE_FOPEN='\"$fopen_file\"'" &> /dev/null 
-test -e ./Installer && make fail || echo '>> BUILD ERROR <<' && exit 8
+make CFLAGS="-I . -I ../src -g -std=c99 -D_DEFAULT_SOURCE -D_GNU_SOURCE -D__AMIGADATE__='\"23.07.1985\"' -DFAIL_LINE_ALLOC=$alloc_line -DFAIL_FILE_ALLOC='\"$alloc_file\"' -DFAIL_LINE_FOPEN=$fopen_line -DFAIL_FILE_FOPEN='\"$fopen_file\"'"
+test -e ./Installer && make fail || exit 1
 ls leak.tmp.* &> /dev/null && cat leak.tmp.* && exit 1
 ls err.tmp.* &> /dev/null && echo $(ls -l err.tmp.* 2> /dev/null | wc -l) "error(s) handled without leaks." && exit 0
 echo "No errors and no leaks."
